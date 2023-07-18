0032aea81b6284ac57efda500461b618279ab52d
laurenma24-meet <139680874+laurenma24-meet@users.noreply.github.com>
2023-07-18 18:29:06 +0300

    ebay website

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -1,13 +1,27 @@
 from flask import Flask, redirect, request, render_template, url_for
 
 
-app = Flask(  # Create a flask app
-    __name__,
-    template_folder='templates',  # Name of html file folder
-    static_folder='static'  # Name of directory for static files
-)
+app = Flask( __name__,template_folder='templates',  static_folder='static' )
 
 # Your code should be below
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/product2')
+def product2():
+    return render_template('product2.html')
+
+
 
 
 
